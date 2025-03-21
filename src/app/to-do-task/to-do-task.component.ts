449660821f7620390e90cac50532c2a30b1c4079
AjvorHardi <markovic.goran@outlook.com>
2025-03-21 06:57:18 -0400

    add to-do task component

---
+++ src/app/to-do-task/to-do-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-task',
+  templateUrl: './to-do-task.component.html',
+  styleUrl: './to-do-task.component.css'
+})
+export class ToDoTaskComponent {
+
+}
